0cb9baf540fdd7eeb6775a2068869250cced59b0
zerohezitation <43836090+zerohezitation@users.noreply.github.com>
2018-11-12 15:00:59 -0800

    Update userparser.cpp

--- userparser.cpp
+++ userparser.cpp
@@ -1,75 +1,92 @@
-/*
- *  2D Graphics Modeler
- *  July 2018
- *
- *  David Epstein <DavidE92@live.com>
- *  Greg Graffius <ggraffius0@saddleback.edu>
- *  Justis Ketcham <justis.ketcham@gmail.com>
- *  Brett Saiki <bsaiki0@saddleback.edu>
- *
- *  Descr:   Final project for CS1C at Saddleback College (Summer 2018)
- *  Teacher: John Kath
- *
- *  Draws shapes using QPainter. Shapes can be loaded from a file, added
- *  to a vector, manipulated and later saved. A login is required to access
- *  the program.
- *
- **/
-/*
-    File: userparser.cpp
-    Parses a file to add users to a users list
- */
+/***********************************************************************
+ * AUTHOR     : Jeffrey Adams
+ * CLASS      : CS1C T/TH 12:30pm-2:50pm
+ **********************************************************************/
 
 #include "userparser.h"
 
+//FUNCTION - CLOSES INPUT FILE
 void UserParser::close()
 {
     if(mInputFile.is_open())
     {
+        //CLOSES INPUT FILE
         mInputFile.close();
+        
+        //CLEARS FILE PATH
         mInputFilePath.clear();
     }
 }
 
+//FUNCTION - LOADS AND PARSES INPUT FILE & RETURNS TRUE IF SUCCESSFUL
 bool UserParser::loadFile(const std::string& filePath)
 {
+    //OPENS INPUT FILE
     mInputFile.open(filePath);
+    
+    //COPY FILE PATH INTO PRIVATE DATA PATH
     mInputFilePath = filePath;
 
     return mInputFile.is_open();
 }
 
+//FUNCTION - LOADS AND PARSES INPUT FILE & RETURNS TRUE IF SUCCESSFUL
 bool UserParser::parse(Vector<User>& userVector)
 {
     if(!mInputFile.is_open())
+    {
         return false;
-
-    User user;
+    }
+    
+    //LOCAL VARIABLES
+    User        user;
     std::string inputLine;
-    int inputType = 0;
+    int         inputType = 0;
 
     while(mInputFile >> inputLine)
     {
         if(inputType == 0)
         {
+            //CLEARS INPUT LINE
             user.mUsername = inputLine.c_str();
+            
+            //INCREMENTS INPUT TYPE
             ++inputType;
         }
         else if(inputType == 1)
         {
+            //CLEARS INPUT LINE
             user.mPassword = inputLine.c_str();
+            
+            //INCREMENTS INPUT TYPE
             ++inputType;
         }
         else
         {
-            if(inputLine == "0")        user.mIsAdmin = false;
-            else if(inputLine == "1")   user.mIsAdmin = true;
-            else                        return false;
+            if(inputLine == "0")
+            {
+                user.mIsAdmin = false;
+            }
+            else if(inputLine == "1")
+            {
+                user.mIsAdmin = true;
+            }
+            else
+            {
+                return false;
+            }
 
             userVector.push_back(user);
+            
+            //CLEARS USERNAME
             user.mUsername.clear();
+            
+            //CLEARS PASSWORD
             user.mPassword.clear();
+            
+            //CLEARS ADMIN
             user.mIsAdmin = 0;
+            
             inputType = 0;
         }
     }
