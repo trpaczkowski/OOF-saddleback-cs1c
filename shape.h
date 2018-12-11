76742791e8df87fdf79143151c89ad4e73dcf59f
17lopezo1 <44882307+17lopezo1@users.noreply.github.com>
2018-12-11 00:45:40 -0800

    Update shape.h

--- shape.h
+++ shape.h
@@ -1,3 +1,25 @@
+/*
+ *  2D Graphics Modeler
+ *  July 2018
+ *
+ *  David Epstein <DavidE92@live.com>
+ *  Greg Graffius <ggraffius0@saddleback.edu>
+ *  Justis Ketcham <justis.ketcham@gmail.com>
+ *  Brett Saiki <bsaiki0@saddleback.edu>
+ *
+ *  Descr:   Final project for CS1C at Saddleback College (Summer 2018)
+ *  Teacher: John Kath
+ *
+ *  Draws shapes using QPainter. Shapes can be loaded from a file, added
+ *  to a vector, manipulated and later saved. A login is required to access
+ *  the program.
+ *
+ **/
+/*
+    File: shape.h
+    Base class of all renderable objects
+*/
+
 #ifndef SHAPE_H
 #define SHAPE_H
 
